3879c421b3ac75817e0e273f92e120dfc5964280
azmajafri <61407560+azmajafri@users.noreply.github.com>
2020-03-13 21:42:05 +0800

    Add files via upload

---
+++ Main.mjs
@@ -0,0 +1,29 @@
+import {Student} from './Student.mjs';
+import readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Name: ', (name) => {
+rl.question('MatricNo: ', (matricNo) => {
+rl.question('Major: ', (major) => {
+
+student.close();
+
+let student = new Student();
+student.name = name;
+student.matricNo = matricNo;
+student.major = major;
+
+display = () => {
+    console.log(`Your Name : ${name}`);
+    console.log(`Your Name : ${matricNo}`);
+    console.log(`Your Name : ${major}`);
+};
+
+});
+});
+});
+
